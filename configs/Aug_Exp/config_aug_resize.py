deb43f9c3c4ac783a9b1ff3f907248b1e3dbaf9a
yxf975 <yxff975@gmail.com>
2022-01-21 12:25:37 +0100

    fix

--- configs/Aug_Exp/config_aug_resize.py
+++ configs/Aug_Exp/config_aug_resize.py
@@ -144,7 +144,7 @@ test_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(
         type='MultiScaleFlipAug',
-        img_scale=(768, 768),  # (1280, 1280),
+        img_scale=[(1333, 1333), (800, 800)],  # (1280, 1280),
         flip=False,
         transforms=[
             dict(type='Resize', keep_ratio=True),
@@ -202,11 +202,11 @@ log_config = dict(
         dict(type='TextLoggerHook'),
         dict(type='WandbLoggerHook',  # wandb logger
              init_kwargs=dict(project='sartorius-aug-exp',
-                              name=f'mask_rcnn-resnet50-768x768-fold0',
+                              name=f'aug-resize',
                               config={'config': 'mask_rcnn_r50_fpn_1x_coco',
                                       'exp_name': 'mask_rcnn-resnet50-aug-exp',
                                       'comment': 'baseline',
-                                      'batch_size': 12,
+                                      'batch_size': 4,
                                       'lr': 0.020
                                       },
                               group='exp_name',
