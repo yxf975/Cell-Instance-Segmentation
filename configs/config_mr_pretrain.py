7995fba1d21a5f922976d1ffa25d65efdcb6b214
yxf975 <yxff975@gmail.com>
2022-01-23 21:18:40 +0100

    fix

--- configs/config_mr_pretrain.py
+++ configs/config_mr_pretrain.py
@@ -159,7 +159,7 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    samples_per_gpu=4,  # BATCH_SIZE
+    samples_per_gpu=2,  # BATCH_SIZE
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
