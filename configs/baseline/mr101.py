d6db322c9c969e75c97afa8344226caec212e479
yxf975 <yxff975@gmail.com>
2022-02-03 10:28:25 +0100

    fix

--- configs/baseline/mr101.py
+++ configs/baseline/mr101.py
@@ -218,6 +218,6 @@ custom_hooks = [dict(type='NumClassCheckHook')]
 
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = 'https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_x101_32x4d_fpn_1x_coco/mask_rcnn_x101_32x4d_fpn_1x_coco_20200205-478d0b67.pth'
+load_from = 'https://download.openmmlab.com/mmdetection/v2.0/mask_rcnn/mask_rcnn_r101_fpn_mstrain-poly_3x_coco/mask_rcnn_r101_fpn_mstrain-poly_3x_coco_20210524_200244-5675c317.pth'
 resume_from = None
 workflow = [('train', 1)]
