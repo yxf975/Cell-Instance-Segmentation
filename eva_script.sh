deb64223b19a863178c49d91de11ab43a570e268
yxf975 <yxff975@gmail.com>
2022-02-17 19:07:23 +0100

    test

--- eva_script.sh
+++ eva_script.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
-cd /bhome/jgeng/Projects/CellSegmentation/Cell-Instance-Segmentation;
+#!/usr/bin/env bash
 
 CONFIG=$1
 MODEL=$2
+echo "第一个参数为：$1";
+echo "第二个参数为：$2";
 
-python3 -m ./evaluation/hp_tuning.py $CONFIG $MODEL &&
-python3 -m ./evaluation/evalution.py $CONFIG $MODEL
+#python3 -m ./evaluation/hp_tuning.py $CONFIG $MODEL &&
+#python3 -m ./evaluation/evalution.py $CONFIG $MODEL
