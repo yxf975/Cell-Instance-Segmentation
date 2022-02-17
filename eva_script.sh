d899bd39b1000fa1ee61740d16ed577b4a6bfd98
yxf975 <yxff975@gmail.com>
2022-02-17 19:02:11 +0100

    fix

--- eva_script.sh
+++ eva_script.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/sh
+cd /bhome/jgeng/Projects/CellSegmentation/Cell-Instance-Segmentation;
 
 CONFIG=$1
 MODEL=$2
 
-python -m ./evaluation/hp_tuning.py $CONFIG $MODEL
-python -m ./evaluation/evalution.py $CONFIG $MODEL
+python3 -m ./evaluation/hp_tuning.py $CONFIG $MODEL &&
+python3 -m ./evaluation/evalution.py $CONFIG $MODEL
