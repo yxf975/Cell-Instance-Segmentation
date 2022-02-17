b457e4cd10e2cef00f6f12fb45eb3af2850a4588
yxf975 <yxff975@gmail.com>
2022-02-17 19:25:36 +0100

    fix

--- eva_script.sh
+++ eva_script.sh
@@ -6,4 +6,4 @@ echo "config path：$CONFIG";
 echo "model path：$MODEL";
 
 python ./evaluation/hp_tuning.py $CONFIG $MODEL &&
-python ./evaluation/evaluation.py $CONFIG $MODEL &&
+python ./evaluation/evaluation.py $CONFIG $MODEL
