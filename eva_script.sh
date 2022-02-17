391115e9038643da23093da536e78b5cbc98c729
yxf975 <yxff975@gmail.com>
2022-02-17 19:10:04 +0100

    test

--- eva_script.sh
+++ eva_script.sh
@@ -5,5 +5,5 @@ MODEL=$2
 echo "第一个参数为：$CONFIG";
 echo "第二个参数为：$MODEL";
 
-#python3 -m ./evaluation/hp_tuning.py $CONFIG $MODEL &&
-#python3 -m ./evaluation/evalution.py $CONFIG $MODEL
+python -m ./evaluation/hp_tuning.py $CONFIG $MODEL &&
+python -m ./evaluation/evalution.py $CONFIG $MODEL
