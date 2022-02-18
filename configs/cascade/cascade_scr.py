d002f790c3d6492c999219c11aae50a55f7f99ca
yxf975 <yxff975@gmail.com>
2022-02-18 16:16:11 +0100

    fix

--- configs/cascade/cascade_scr.py
+++ configs/cascade/cascade_scr.py
@@ -311,7 +311,7 @@ log_config = dict(
         dict(type='TextLoggerHook'),
         dict(type='WandbLoggerHook',  # wandb logger
              init_kwargs=dict(project='sartorius-model-eva',
-                              name=f'cascade-pretrain',
+                              name=f'cascade-scr',
                               config={'config': 'mask_rcnn_r50_fpn_1x_coco',
                                       'exp_name': 'mask_rcnn-resnet50-aug-exp',
                                       'comment': 'baseline',
@@ -325,9 +325,7 @@ log_config = dict(
 custom_hooks = [dict(type='NumClassCheckHook')]
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-load_from = 'https://download.openmmlab.com/mmdetection/v2.0/cascade_rcnn' \
-            '/cascade_mask_rcnn_x101_64x4d_fpn_mstrain_3x_coco' \
-            '/cascade_mask_rcnn_x101_64x4d_fpn_mstrain_3x_coco_20210719_210311-d3e64ba0.pth '
+load_from = None
 resume_from = None
 workflow = [('train', 1)]
 seed = 0
