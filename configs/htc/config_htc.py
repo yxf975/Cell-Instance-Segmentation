f2c7728a1fab103d64e9c931381014cad96d3cf7
yxf975 <yxff975@gmail.com>
2022-02-21 00:41:05 +0100

    fix

--- configs/htc/config_htc.py
+++ configs/htc/config_htc.py
@@ -357,7 +357,7 @@ swa_lr_config = dict(
     target_ratio=(1, 0.01),
     cyclic_times=12,
     step_ratio_up=0.0)
-swa_runner = dict(type='EpochBasedRunner', max_epochs=12)
+swa_runner = dict(type='EpochBasedRunner', max_epochs=total_epochs)
 # the epoch interval to perform swa
 swa_interval = 1
 
